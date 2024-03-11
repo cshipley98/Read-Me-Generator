31fed8d5853accdb321fc26ec9fab04f6602c8b9
cshipley98 <claireshipley98@gmail.com>
2024-03-11 09:45:01 -0500

    initial github input

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
