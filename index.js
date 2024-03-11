835a5163f7f9724b025fa7f293a935c6dba2b161
cshipley98 <claireshipley98@gmail.com>
2024-03-11 09:50:51 -0500

    email imput

--- index.js
+++ index.js
@@ -2,7 +2,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: githubInput => {
+                if (githubInput){
+                    return true;
+
+                } else {
+                    console.log('Enter your GitHub Username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email-address?',
+            validate: emailInput => {
+                if (emailInput){
+                    return true;
+                }   else{
+                    console.log('Enter your e-mail adress');
+                    return false;
+                }
+            }
+        },
+        {
+            
+        }
+
+    ])
+}
 
 
 
