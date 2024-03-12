2cb1536cab0f8f1fb8f8914dd51647adbc105445
cshipley98 <claireshipley98@gmail.com>
2024-03-11 20:53:57 -0500

    starting of function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -15,11 +15,27 @@ if (license === 'none'){
   return licenseBadge + 'GNULGPLv3-brightorange';
 } else if (license === 'Apache 2.0'){
   return licenseBadge + 'ApacheLicense2.0-brightgreen';
+} else if (license === 'Mozilla 2.0'){
+  return licenseBadge + 'MozillaPublicLicense2.0-yellow'
+} else if (license === 'Boost Software'){
+  licenseOption = 'BootSoftwareLicense1.0';
+  return licenseBadge +'BoostSoftwareLicense1.0-blueviolet';
+} else if (license === 'Unlicense'){
+  return licenseBadge + TheUnlicense-yellowgreen;
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license => {
+  let licenseLink = 'https://choosealicense.com/licenses/';
+//return an empty string
+if (license === 'None'){
+  return licenseKink = '';
+} else if (license === 'MIT'){
+  return licenseLink = licenseLink + 'mit/';
+} else if (license === 'GNU AGPLv3'){
+  
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
